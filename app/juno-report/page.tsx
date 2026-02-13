b4cf27e91d20fa62c278933385fe6d6b7fc30206
Agaaz Singhal <agaazsinghal@gmail.com>
2026-02-13 02:20:31 -0800

    new juno report

---
+++ app/juno-report/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export default function JunoReportPage() {
+  return (
+    <iframe
+      src="/juno-report.html"
+      className="w-full h-screen border-0"
+      title="Juno Growth & Friction Analysis Report"
+    />
+  )
+}
